5058b80a182d7073a9107e90519163eb1880f705
Allan DeBoe <adeboe@asu.edu>
2022-10-07 21:43:30 -0700

    Updated prototype\urls.py
    I changed it as I forgot to make the changes necessary due to moving files from one virtual environment (venv) to another, and that the Django app on each venv labels certain things differently.

--- task-6-ui-back-end/prototype/prototype/urls.py
+++ task-6-ui-back-end/prototype/prototype/urls.py
@@ -25,12 +25,12 @@ Including another URLconf
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 from django.contrib import admin
 from django.urls import include, path
-from django.conf.urls import url
+#from django.conf.urls import url
 
-from prototype.views import *
+from core.views import *
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
     path('react-test/', ReactView.as_view(), name="React Test")
 ]
